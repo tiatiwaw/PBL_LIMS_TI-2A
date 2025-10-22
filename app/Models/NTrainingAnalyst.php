7d1862c4c9e63156dcb05b7cb1411a54ad4c318d
Nabila310 <ramadaninabilaa@gmail.com>
2025-10-22 13:58:31 +0700

    feat: make models user, analys, training, certificate, ntraininganalys

--- app/Models/NTrainingAnalyst.php
+++ app/Models/NTrainingAnalyst.php
@@ -2,9 +2,37 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class NTrainingAnalyst extends Model
 {
-    //
+    use HasFactory;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'n_training_analysts';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'analyst_id',
+        'training_id',
+    ];
+
+    public function analyst()
+    {
+        return $this->belongsTo(Analyst::class);
+    }
+
+    public function training()
+    {
+        return $this->belongsTo(Training::class);
+    }
 }
