633a2f5963528bc7dd404bd96b645173366ed0fa
Raki <rakiabhistaprakoso@gmail.com>
2025-11-06 19:51:56 +0700

    Certificate Model

--- database/migrations/2025_10_14_020230_create_certificates_table.php
+++ database/migrations/2025_10_14_020230_create_certificates_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('name');
             $table->date('issued_date');
             $table->date('expired_date');
-            $table->string('filed_path');
+            $table->string('file_path');
             $table->timestamps();
         });
     }
