0150186b9f10615d8168efc7560bcb4ee464e6dc
ahmad chomsin <ahmadruddin17@gmail.com>
2025-11-08 11:42:46 +0700

    refactor: update dashboard API endpoint in adminService to use base service method

--- resources/js/services/adminService.js
+++ resources/js/services/adminService.js
@@ -1,6 +1,6 @@
 import { serviceMethods } from "./baseService";
 
 export const adminService = {
-    dashboard: serviceMethods("/admin/dashboard"),
+    dashboard: serviceMethods("/admin"),
     brands: serviceMethods("/admin/tools/brands"),
 };
