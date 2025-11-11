18f701bd443449f67640986a62ccee2c4397345d
ImNotDanish05 <danishmahdi2005@gmail.com>
2025-11-11 08:51:51 +0700

    Update: Add Empty files about Orders and Users API

---
+++ app/Http/Controllers/API/V1/Manager/UsersController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1\Manager;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class UsersController extends Controller
+{
+    //
+}
