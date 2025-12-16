aac0aa6a763041850c03b5c53b32861a1d715eae
Algaray02 <alfin.rozzaq.270206@gmail.com>
2025-12-16 21:10:27 +0700

    fix: update backRoute in OrderDetailHeader to point to the correct path

--- resources/js/pages/manager/detail/index.jsx
+++ resources/js/pages/manager/detail/index.jsx
@@ -118,7 +118,7 @@ export default function ManagerDetailOrder() {
             <div className="max-w-7xl mx-auto space-y-6">
                 <OrderDetailHeader 
                     order={order} 
-                    backRoute="/supervisor/orders/follow-up"
+                    backRoute="/manager/report_validations"
                 />
 
                 <ClientInfoCard client={order.clients} />
