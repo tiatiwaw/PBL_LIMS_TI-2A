28b87de249677b02fc7d00ef276100dc15773b5b
Algaray02 <alfin.rozzaq.270206@gmail.com>
2025-12-16 21:13:42 +0700

    update bug

--- resources/js/pages/manager/detail/index.jsx
+++ resources/js/pages/manager/detail/index.jsx
@@ -118,7 +118,7 @@ export default function ManagerDetailOrder() {
             <div className="max-w-7xl mx-auto space-y-6">
                 <OrderDetailHeader 
                     order={order} 
-                    backRoute="/manager/report_validations"
+                    backRoute="/manager/report-validations"
                 />
 
                 <ClientInfoCard client={order.clients} />
